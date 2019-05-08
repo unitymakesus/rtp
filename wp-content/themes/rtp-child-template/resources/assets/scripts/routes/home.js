9a455982bfec41fcfa70dd134e22727754c444b2
Alisa Herr <alisa@unitymakes.us>
2019-05-07 20:44:30 -0400

    Loading animation: Expand bar

--- wp-content/themes/rtp-child-template/resources/assets/scripts/routes/home.js
+++ wp-content/themes/rtp-child-template/resources/assets/scripts/routes/home.js
@@ -12,13 +12,15 @@ export default {
     // Build tween
     var tween = new TimelineMax();
     tween.to('#svg-where', 1, {autoAlpha:0}, 0).to('#svg-convene', 1, {autoAlpha:0}, 0).to('#svg-plus', 1, {autoAlpha:0}, 0)
-      .to('#svg-people', 2, {attr:{x:50}}, 0).to('#svg-ideas', 2, {attr:{dx:-100}}, 0)
+      .to('#svg-people', 3, {attr:{x:50}}, 0).to('#svg-ideas', 3, {attr:{dx:-100}}, 0)
+      .to('#gradient-bar', 4, {width:'100%'}, 1);
+      // .setClassToggle('#gradient-bar', 'wide')
       // .to('#svg-people', 1, {text:{value:'RTP', delimiter:' '}}, 1)
       // .to('#svg-people', 1, {autoAlpha:0}, 1)
       // .to('#svg-ideas', 1, {autoAlpha:0}, 1);
 
     // Build scene
-    new ScrollMagic.Scene({triggerElement:'#landing-graphic', offset:400, duration:'100%'}).setTween(tween).setPin('#svg-text', {pushFollowers: false}).addTo(controller);
+    new ScrollMagic.Scene({triggerElement:'#landing-graphic', offset:250, duration:'100%'}).setTween(tween).setPin('#landing-graphic-pin', {pushFollowers: false}).addTo(controller);
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
--- wp-content/themes/rtp-child-template/resources/views/front-page.blade.php
+++ wp-content/themes/rtp-child-template/resources/views/front-page.blade.php
@@ -4,24 +4,30 @@
   @while(have_posts()) @php the_post() @endphp
     <article {!! post_class() !!}>
 
-      <div id="landing-graphic" class="landing-graphic">
-        <svg id="svg-text" viewBox="0 0 324 132">
-          <defs>
-            <linearGradient id="rtp-gradient" x1="0" x2="100%" y1="0" y2="0" gradientUnits="userSpaceOnUse" >
-              <stop stop-color="#4B9CD3" offset="0%"/>
-              <stop stop-color="#012169" offset="25%"/>
-              <stop stop-color="#82052A" offset="50%"/>
-              <stop stop-color="#CC0000" offset="75%"/>
-            </linearGradient>
-          </defs>
-          <text fill="url(#rtp-gradient)">
-            <tspan id="svg-where" font-size="40" x="10" y="40">Where</tspan>
-            <tspan id="svg-people" font-size="40" x="10" dy="35">People</tspan>
-            <tspan id="svg-plus" font-size="40" dx="0" dy="0">+</tspan>
-            <tspan id="svg-ideas" font-size="40" dx="0" dy="0">Ideas</tspan>
-            <tspan id="svg-convene" font-size="40" x="10" dy="35">Convene</tspan>
-          </text>
-        </svg>
+      <div class="spacer"></div>
+
+      <div id="landing-graphic">
+        <div id="landing-graphic-pin" class="landing-graphic-scene">
+          <svg id="svg-text" viewBox="0 0 325 130">
+            <defs>
+              <linearGradient id="rtp-gradient" x1="0" x2="100%" y1="0" y2="0" gradientUnits="userSpaceOnUse" >
+                <stop stop-color="#4B9CD3" offset="0%"/>
+                <stop stop-color="#012169" offset="25%"/>
+                <stop stop-color="#82052A" offset="50%"/>
+                <stop stop-color="#CC0000" offset="75%"/>
+              </linearGradient>
+            </defs>
+            <text fill="url(#rtp-gradient)">
+              <tspan id="svg-where" font-size="40" x="10" y="40">Where</tspan>
+              <tspan id="svg-people" font-size="40" x="10" dy="35">People</tspan>
+              <tspan id="svg-plus" font-size="40" dx="6" dy="0">+</tspan>
+              <tspan id="svg-ideas" font-size="40" dx="6" dy="0">Ideas</tspan>
+              <tspan id="svg-convene" font-size="40" x="10" dy="35">Convene</tspan>
+            </text>
+          </svg>
+
+          <div id="gradient-bar"></div>
+        </div>
       </div>
 
       <section class="placeholder"></section>
