cdc5b6c89d79e3459e02734190b51c522be48fb1
lexinamer <lexinamer@gmail.com>
2019-07-23 15:19:35 -0400

    adding page header

--- wp-content/themes/rtp-core/resources/views/partials/page-header.blade.php
+++ wp-content/themes/rtp-core/resources/views/partials/page-header.blade.php
@@ -1,3 +1,7 @@
-<header class="page-header">
-  <h1>{!! App::title() !!}</h1>
+<?php $feat_img = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); ?>
+
+<header class="page-header" style="background-image: url('<?php echo $feat_img[0]; ?>')">
+  <div class="container-wide">
+    <h1>{!! App::title() !!}</h1>
+  </div>
 </header>
