7dddbd9c0e85a6c6e2bb519ed6d863d5c4cc22f5
Alisa Herr <alisa@unitymakes.us>
2019-07-24 20:25:14 -0400

    Create plugin to import food trucks from google sheet into MEC plugin

--- wp-content/plugins/mec-g-import-food-trucks/mec-g-import-food-trucks.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * Plugin Name: MEC - Import Food Trucks from Google Calendar
- * Description: Cron job for this is located in cPanel
- * Version: 1.0
- * Author: Unity Digital Agency
- */
