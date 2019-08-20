81f3053cc750efa9cdca6110837cf1e5485a1717
Alisa Herr <alisa@unitymakes.us>
2019-08-20 19:36:08 -0400

    Clean up child theme style overrides

--- wp-content/themes/rtp-child-rtp/resources/views/front-page.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-  @while(have_posts()) @php the_post() @endphp
-    <article {!! post_class() !!}>
-      @include('partials.content-page')
-    </article>
-  @endwhile
-@endsection
