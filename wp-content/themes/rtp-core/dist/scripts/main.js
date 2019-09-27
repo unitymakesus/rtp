5f6935747c9b63de77c10c3b964f2ac10cc4e43f
Alisa Herr <alisa@unitymakes.us>
2019-09-27 17:46:48 -0400

    Add mega links to cards

--- wp-content/themes/rtp-core/dist/scripts/main.js
+++ wp-content/themes/rtp-core/dist/scripts/main.js
@@ -3616,8 +3616,18 @@ Router.prototype.loadEvents = function loadEvents () {
 "use strict";
 /* WEBPACK VAR INJECTION */(function($) {/* harmony default export */ __webpack_exports__["a"] = ({
   init: function init() {
+    // Materialize Select Dropdowns
     $('.mec-date-search select').formSelect();
     $('.mec-dropdown-search select').formSelect();
+
+    // Search form for any URL params that are set on load
+    // var url = new URL(url_string);
+    // var category = url.searchParams.get("category");
+    // var location = url.searchParams.get("location");
+    // var type = url.searchParams.get("type");
+    //
+    // console.log(category);
+
   },
 });
 
