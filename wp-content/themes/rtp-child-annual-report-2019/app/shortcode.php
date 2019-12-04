f48181715553d5d055d93adade94711e2623df9a
Andrew Marino <armarino@ad.unc.edu>
2019-12-04 14:13:38 -0500

    install lordicon plugin, minor adjustments per feedback

--- wp-content/themes/rtp-child-annual-report-2019/app/shortcode.php
+++ wp-content/themes/rtp-child-annual-report-2019/app/shortcode.php
@@ -9,25 +9,25 @@ add_shortcode('annual-report-boxyard', function($atts) {
   ob_start();
   ?>
   <section id="boxyard">
-    <div class="map" style="background-image:url('/wp-content/themes/rtp-child-annual-report-2019/dist/images/boxyard-rtp-map_c8a63222.png');"></div>
+    <div class="map" style="background-image:url('<?= asset_path('images/boxyard-rtp-map.png'); ?>');"></div>
     <div class="boxes">
       <div class="box box-1" tabindex="0">
         <img src="https://placehold.it/300/" alt="" />
       </div>
       <div class="box box-2" tabindex="0">
-        <img src="/wp-content/themes/rtp-child-annual-report-2019/dist/images/logos/logo-uncorked@2x_6995ab4b.png" alt="" />
+        <img src="<?= asset_path('images/logos/logo-uncorked.png'); ?>" alt="RTP Uncorked" />
       </div>
       <div class="box box-3"></div>
       <div class="box box-4" tabindex="0">
-        <img src="/wp-content/themes/rtp-child-annual-report-2019/dist/images/logos/logo-fullsteam@2x_7a1328d0.png" alt="" />
+        <img src="<?= asset_path('images/logos/logo-fullsteam.png'); ?>" alt="Fullsteam Brewery" />
       </div>
       <div class="box box-5"></div>
       <div class="box box-6" tabindex="0">
-        <img src="/wp-content/themes/rtp-child-annual-report-2019/dist/images/logos/logo-medicinemamas_eab88541.png" alt="" />
+        <img src="<?= asset_path('images/logos/logo-medicinemamas.png'); ?>" alt="Medicine Mamas" />
       </div>
       <div class="box box-7"></div>
       <div class="box box-8" tabindex="0">
-        <img src="/wp-content/themes/rtp-child-annual-report-2019/dist/images/logos/logo-pressedpoured@2x_91b9dfb2.png" alt="" />
+        <img src="<?= asset_path('images/logos/logo-pouredpressed.png'); ?>" alt="Poured & Pressed" />
       </div>
       <div class="box box-9" tabindex="0">
         <img src="https://placehold.it/300/" alt="" />
