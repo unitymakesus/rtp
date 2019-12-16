b5df54a3d52c1d6401087ecd53b7e3d6da56bb50
Andrew Marino <andrew@andrewmarino.net>
2019-12-16 16:48:43 -0500

    small a11y fixes and link improvements on figure cards

--- wp-content/themes/rtp-child-annual-report-2019/app/shortcode.php
+++ wp-content/themes/rtp-child-annual-report-2019/app/shortcode.php
@@ -9,6 +9,7 @@ add_shortcode('annual-report-boxyard', function($atts) {
   ob_start();
   ?>
   <section id="boxyard">
+    <span class="screen-reader-text"><?= __('These vendors coming soon to Boxyard RTP!'); ?></span>
     <div class="map" style="background-image:url('<?= asset_path('images/boxyard-rtp-map.png'); ?>');"></div>
     <div class="boxes">
       <div class="box box-1" tabindex="0">
@@ -29,8 +30,8 @@ add_shortcode('annual-report-boxyard', function($atts) {
       <div class="box box-8" tabindex="0">
         <img src="<?= asset_path('images/logos/logo-pouredpressed.png'); ?>" alt="Poured & Pressed" />
       </div>
-      <div class="box box-9" tabindex="0">
-        <img src="<?= asset_path('images/logos/logo-comingsoon.png'); ?>" alt="Coming Soon" />
+      <div class="box box-9" aria-hidden="true">
+        <img src="<?= asset_path('images/logos/logo-comingsoon.png'); ?>" alt="" />
       </div>
     </div>
   </section>
