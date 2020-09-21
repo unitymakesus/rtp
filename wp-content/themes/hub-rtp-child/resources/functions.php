c35a04b4900ae81c39f4791740446e817a1b761c
Andy Marino <andy@unitywebagency.com>
2020-09-21 17:21:57 -0400

    add distributor functions to hub child theme

--- wp-content/themes/hub-rtp-child/resources/functions.php
+++ wp-content/themes/hub-rtp-child/resources/functions.php
@@ -3,10 +3,27 @@
 namespace App;
 
 /**
- * Theme assets
+ * Helper function for prettying up errors
+ * @param string $message
+ * @param string $subtitle
+ * @param string $title
  */
-add_action('wp_enqueue_scripts', function () {
-  // Enqueue scripts and styles
-  wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
-  wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
-}, 100);
+$sage_error = function ($message, $subtitle = '', $title = '') {
+    $title = $title ?: __('Sage &rsaquo; Error', 'sage');
+    $footer = '<a href="https://roots.io/sage/docs/">roots.io/sage/docs/</a>';
+    $message = "<h1>{$title}<br><small>{$subtitle}</small></h1><p>{$message}</p><p>{$footer}</p>";
+    wp_die($message, $title);
+};
+
+/**
+ * Sage required files
+ *
+ * The mapped array determines the code library included in your theme.
+ * Add or remove files to the array as needed. Supports child theme overrides.
+ */
+array_map(function ($file) use ($sage_error) {
+    $file = "../app/{$file}.php";
+    if (!locate_template($file, true, true)) {
+        $sage_error(sprintf(__('Error locating <code>%s</code> for inclusion.', 'sage'), $file), 'File not found');
+    }
+}, ['distributor']);
