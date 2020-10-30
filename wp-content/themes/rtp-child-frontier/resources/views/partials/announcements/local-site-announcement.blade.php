3565d58ef12805714c99f4634fde6efcac62ed68
Andy Marino <andy@unitywebagency.com>
2020-10-30 14:40:52 -0400

    adjust logic for site announcement on frontier

--- wp-content/themes/rtp-child-frontier/resources/views/partials/announcements/local-site-announcement.blade.php
+++ wp-content/themes/rtp-child-frontier/resources/views/partials/announcements/local-site-announcement.blade.php
@@ -1,9 +1,17 @@
-@if ($announcement = App::localSiteAnnouncement())
+@php
+
+$announcement = App::localSiteAnnouncement();
+$headcount = get_field('local_coworking_floor_headcount', 'option');
+
+@endphp
+
+@if ($announcement || $headcount)
   <div class="announcement announcement--notice">
     <div class="container">
-      {!! $announcement !!}
-
-      @if ($headcount = get_field('local_coworking_floor_headcount', 'option'))
+      @if (!empty($announcement))
+        {!! $announcement !!}
+      @endif
+      @if ($headcount)
         <div class="announcement__headcount">
           <strong>{{ __('Remaining Coworking Seats Available: ', 'sage') }}</strong>
           <span class="announcement__headcount-num">{{ $headcount }}</span>
