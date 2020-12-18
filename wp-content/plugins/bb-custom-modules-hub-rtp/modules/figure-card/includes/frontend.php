6559b082b29ac544177685ff4dfb922d3542c8d0
Andy Marino <andy@unitywebagency.com>
2020-12-18 16:27:47 -0500

    remove alt from figure card image args
    we'll let the alt text come in naturally from the alt text set on the attachment

--- wp-content/plugins/bb-custom-modules-hub-rtp/modules/figure-card/includes/frontend.php
+++ wp-content/plugins/bb-custom-modules-hub-rtp/modules/figure-card/includes/frontend.php
@@ -1,6 +1,6 @@
 <div class="hub-rtp-figure-card hub-rtp-figure-card--image-align-<?php echo $settings->image_align; ?>">
     <figure class="hub-rtp-figure-card__image">
-        <?php echo wp_get_attachment_image($settings->image, 'large', false, ['alt' => $settings->image_alt]); ?>
+        <?php echo wp_get_attachment_image($settings->image, 'large'); ?>
     </figure>
     <div class="hub-rtp-figure-card__content">
         <?php echo $settings->content; ?>
