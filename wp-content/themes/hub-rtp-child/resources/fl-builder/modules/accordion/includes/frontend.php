819437d5b9585f93fa8fc4834bada87f68ee1c21
Andy Marino <andy@unitywebagency.com>
2020-12-18 09:42:22 -0500

    minor style updates for fl layouts

--- wp-content/themes/hub-rtp-child/resources/fl-builder/modules/accordion/includes/frontend.php
+++ wp-content/themes/hub-rtp-child/resources/fl-builder/modules/accordion/includes/frontend.php
@@ -10,16 +10,17 @@
             $label_id   = 'fl-accordion-' . $module->node . '-label-' . $i;
             $icon_id    = 'fl-accordion-' . $module->node . '-icon-' . $i;
             $content_id = 'fl-accordion-' . $module->node . '-panel-' . $i;
+            $control_id = 'fl-accordion-' . $module->node . '-control-' . $i;
         ?>
         <h3>
-            <button class="fl-accordion-trigger" <?php echo (!empty($settings->id)) ? ' id="' . sanitize_html_class($settings->id) . '-' . $i . '"' : ''; ?> aria-expanded="false" aria-controls="<?php echo $content_id; ?>">
+            <button class="fl-accordion-trigger" id="<?php echo $control_id; ?>" aria-expanded="false" aria-controls="<?php echo $content_id; ?>">
                 <span class="fl-accordion-title">
                     <?php echo $settings->items[$i]->label; ?>
                     <span class="fl-accordion-icon"></span>
                 </span>
             </button>
         </h3>
-        <div class="fl-accordion-panel" id="<?php echo $content_id; ?>" role="region" aria-labelledby="<?php echo 'fl-accordion-' . $module->node . '-tab-' . $i; ?>" hidden>
+        <div class="fl-accordion-panel" id="<?php echo $content_id; ?>" role="region" aria-labelledby="<?php echo $control_id; ?>" hidden>
             <div>
                 <?php echo wpautop($wp_embed->autoembed($settings->items[$i]->content)); ?>
             </div>
