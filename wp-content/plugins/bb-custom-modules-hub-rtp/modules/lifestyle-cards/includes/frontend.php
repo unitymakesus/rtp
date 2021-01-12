41592e021b600a2f5f3af1a4c33b0ecb8309e86e
Andrew Marino <andy@unitywebagency.com>
2021-01-12 08:43:46 -0500

    revisions and finesse to allow a11y-link-wraps on card toggles
    also minor animations

--- wp-content/plugins/bb-custom-modules-hub-rtp/modules/lifestyle-cards/includes/frontend.php
+++ wp-content/plugins/bb-custom-modules-hub-rtp/modules/lifestyle-cards/includes/frontend.php
@@ -8,11 +8,14 @@
             ?>
             <div class="cbb-lifestyle__card">
                 <div class="cbb-lifestyle__card-inner">
-                    <button class="cbb-lifestyle__card-toggle" aria-expanded="false" aria-describedby="lifestyle-card-<?php echo $i; ?>">
-                    <svg viewBox="0 0 10 10"><path d="m1 4h8v2h-8zm3-3h2v8h-2z" fill="#FFFFFF"/></svg>
-                    </button>
+                    <a role="button" href="#" class="cbb-lifestyle__card-toggle a11y-link-wrap" aria-expanded="false" aria-describedby="lifestyle-card-<?php echo $i; ?>">
+                        <span>
+                            <svg viewBox="0 0 10 10"><path d="m1 4h8v2h-8zm3-3h2v8h-2z" fill="#FFFFFF"/></svg>
+                        </span>
+                    </a>
                     <div class="cbb-lifestyle__card-body">
-                        <div class="cbb-lifestyle__card-front" style="background-image:url(<?php echo wp_get_attachment_image_src($settings->cards[$i]->background_image, 'large')[0]; ?>);">
+                        <div class="cbb-lifestyle__card-front">
+                            <div class="cbb-lifestyle__card-front-img" style="background-image:url(<?php echo wp_get_attachment_image_src($settings->cards[$i]->background_image, 'large')[0]; ?>);"></div>
                         </div>
                         <div class="cbb-lifestyle__card-back">
                             <div class="cbb-lifestyle__card-back-inner">
